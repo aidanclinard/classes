b561797ebe0bc8a65762fa704e18696b3bd6d3bd
aidanclinard <aidanclinard@gmail.com>
2023-07-11 11:20:18 -0700

    Added seperate classes

--- week-4-main/prove/Develop04/Program.cs
+++ week-4-main/prove/Develop04/Program.cs
@@ -2,11 +2,10 @@ using System;
 
 class Program
 {
-
-    static void Main(){
+        static void Main(){
         Console.Clear();
         Console.WriteLine("Welcome, your activities will be laid out here.");
-        dot_loaidng_anim(3);
+        Animations.dot_loaidng_anim(3);
         Console.Clear();
         Console.WriteLine("Where would you like to start?");
         Thread.Sleep(500);
@@ -26,70 +25,27 @@ class Program
             Console.WriteLine("Incorrect selection");
         }
         if(choice == 1){
-            breathing_exercises();
+            Games.breathing_exercises();
         }
         if(choice == 2){
-            reflection();
+            Games.reflection();
         }
         if(choice == 3){
-            math_game();
+            Games.math_game();
         }
         if(choice != 1 && choice != 2 && choice != 3){
             Console.WriteLine("Incorrect selection");
         }
     }
-
-    static void breathing_exercises(){
-        Console.Clear();
-        Console.WriteLine("Welcome to the breathing exercises activity.");
-        Thread.Sleep(1000);
-        Console.WriteLine("I will tell you to either inhale or exhale. This will help you calm down.");
-        dot_loaidng_anim(3);
-        Console.Clear();
-        Console.WriteLine("Let's begin.");
-        Thread.Sleep(1000);
-        Console.Clear();
-        for(int i = 0; i < 10; i++)
-        {
-            Console.WriteLine("Inhale");
-            Thread.Sleep(4000);
-            Console.Clear();
-            Console.WriteLine("Exhale");
-            Thread.Sleep(4000);
-            Console.Clear();
-        }
-        Console.WriteLine("Excelent work, press any key when you are ready to exit.");     
-        Console.ReadKey();   
-    }
-    static void reflection(){
-        Console.Clear();
-        Console.WriteLine("Welcome to reflection, I will ask you a question about your day so I can log it for you. Don't think too hard about it.");
-        dot_loaidng_anim(3);
-        Console.Clear();
-        var prompts = new List<string>{"What did you do today?", "What did you eat today?", "What was the best part of your day?", "If you had one extra thing you could do today, what would it be?"};
-        Random rnd = new Random();
-        int index = rnd.Next(prompts.Count);
-        Console.WriteLine(prompts[index]);
-        string input = Console.ReadLine();
-        File.AppendAllText("journal.txt", "-----------------------------" + Environment.NewLine);
-        File.AppendAllText("journal.txt",DateTime.Now.ToString() + Environment.NewLine);
-        File.AppendAllText("journal.txt",prompts[index] + Environment.NewLine);
-        File.AppendAllText("journal.txt",input + Environment.NewLine);
-        File.AppendAllText("journal.txt","-----------------------------" + Environment.NewLine);
-        Console.WriteLine("Thanks for your response! Here are your previous responses:");
-        string journallog = File.ReadAllText("journal.txt");
-        Console.WriteLine(journallog);
-        dot_loaidng_anim(4);
-        Console.WriteLine("When you are ready, you can press any key to exit. See you next time!");
-        Console.ReadKey();
-    }
-
-    static void math_game(){
+}
+class Games
+{
+   public static void math_game(){
         Console.Clear();
         Console.WriteLine("Let's work our math skills.");
         Thread.Sleep(1000);
         Console.WriteLine("I will ask you twenty math questions, try to answer as fast as possible. There is no time limit, so try to get them correct.");
-        dot_loaidng_anim(4);
+        Animations.dot_loaidng_anim(4);
         for(int i = 0; i < 20; i++)
         {
             Random rnd = new Random();
@@ -140,10 +96,57 @@ class Program
         }
         Console.WriteLine("Excellent work! When you are ready, you can press any key to exit. See you next time!");
         Console.ReadKey();
+    }  
+    public static void reflection(){
+        Console.Clear();
+        Console.WriteLine("Welcome to reflection, I will ask you a question about your day so I can log it for you. Don't think too hard about it.");
+        Animations.dot_loaidng_anim(3);
+        Console.Clear();
+        var prompts = new List<string>{"What did you do today?", "What did you eat today?", "What was the best part of your day?", "If you had one extra thing you could do today, what would it be?"};
+        Random rnd = new Random();
+        int index = rnd.Next(prompts.Count);
+        Console.WriteLine(prompts[index]);
+        string input = Console.ReadLine();
+        File.AppendAllText("journal.txt", "-----------------------------" + Environment.NewLine);
+        File.AppendAllText("journal.txt",DateTime.Now.ToString() + Environment.NewLine);
+        File.AppendAllText("journal.txt",prompts[index] + Environment.NewLine);
+        File.AppendAllText("journal.txt",input + Environment.NewLine);
+        File.AppendAllText("journal.txt","-----------------------------" + Environment.NewLine);
+        Console.WriteLine("Thanks for your response! Here are your previous responses:");
+        string journallog = File.ReadAllText("journal.txt");
+        Console.WriteLine(journallog);
+        Animations.dot_loaidng_anim(4);
+        Console.WriteLine("When you are ready, you can press any key to exit. See you next time!");
+        Console.ReadKey();
+    }    
+    public static void breathing_exercises(){
+        Console.Clear();
+        Console.WriteLine("Welcome to the breathing exercises activity.");
+        Thread.Sleep(1000);
+        Console.WriteLine("I will tell you to either inhale or exhale. This will help you calm down.");
+        Animations.dot_loaidng_anim(3);
+        Console.Clear();
+        Console.WriteLine("Let's begin.");
+        Thread.Sleep(1000);
+        Console.Clear();
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Inhale");
+            Thread.Sleep(4000);
+            Console.Clear();
+            Console.WriteLine("Exhale");
+            Thread.Sleep(4000);
+            Console.Clear();
+        }
+        Console.WriteLine("Excelent work, press any key when you are ready to exit.");     
+        Console.ReadKey();   
     }
+}
 
+class Animations
+{
 
-    static void dot_loaidng_anim(int count){
+    public static void dot_loaidng_anim(int count){
         for(int i = 0; i < count; i++){
             Console.Write(".");
             Thread.Sleep(500);
@@ -162,5 +165,4 @@ class Program
         Console.Write(new string(' ', Console.WindowWidth));
         Console.SetCursorPosition(0, Console.CursorTop);
     }
-    
 }
